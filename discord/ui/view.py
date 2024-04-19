238aaa9ef2d5e67761625111506b076e5431a19b
Roman <157638796+16Hackley@users.noreply.github.com>
2024-04-19 03:41:49 -0700

    Add files via upload

--- discord/ui/view.py
+++ discord/ui/view.py
@@ -615,15 +615,16 @@ class ViewStore:
 
         view = View.from_message(interaction.message, timeout=None)
 
-        try:
-            base_item_index, base_item = next(
-                (index, child)
-                for index, child in enumerate(view._children)
-                if child.type.value == component_type and getattr(child, 'custom_id', None) == custom_id
-            )
-        except StopIteration:
+        base_item_index: Optional[int] = None
+        for index, child in enumerate(view._children):
+            if child.type.value == component_type and getattr(child, 'custom_id', None) == custom_id:
+                base_item_index = index
+                break
+
+        if base_item_index is None:
             return
 
+        base_item = view._children[base_item_index]
         try:
             item = await factory.from_custom_id(interaction, base_item, match)
         except Exception:
@@ -633,7 +634,6 @@ class ViewStore:
         # Swap the item in the view with our new dynamic item
         view._children[base_item_index] = item
         item._view = view
-        item._rendered_row = base_item._rendered_row
         item._refresh_state(interaction, interaction.data)  # type: ignore
 
         try:
